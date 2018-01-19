c831a0744edd3d914bb47f99b0c3a177f4ecebdd
teimurjan <teymurgg321@gmail.com>
2018-01-19 16:41:07 +0600

    Binary gapm solution added

---
+++ binary_gap.py
@@ -0,0 +1,24 @@
+def solution(n):
+    binary_string = '{:b}'.format(n)
+    if len(binary_string) < 3:
+        return 0
+    is_gap_open = False
+    gap_size = 0
+    max_gap_size = 0
+    i = 0
+    while(i < len(binary_string)):
+        si = binary_string[i]
+
+        if si == '1':
+            if is_gap_open:
+                if (gap_size > max_gap_size):
+                    max_gap_size = gap_size
+                gap_size = 0
+                is_gap_open = False
+                continue
+            else:
+                is_gap_open = True
+        else:
+            gap_size += 1
+        i += 1
+    return max_gap_size
